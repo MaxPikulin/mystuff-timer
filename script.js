88c459fdd648834602206eb14308da68d3af6de0
MaxPikulin <trurl1000@gmail.com>
2017-08-12 19:14:21 +0100

    total revamp

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
+var timer = {
+	minutes: 0,
+	seconds: 0,
+	show: function() {
+		var m=this.minutes+'',s=this.seconds+'';
+		if(m.length<2) m='0'+m;
+		if(s.length<2) s='0'+s;
+		return `${m}:${s}`;
+	}
+}
+
 var timerDiv = document.querySelector('#timerInner');
 var startBtn = document.querySelector('#startBtn');
 var resetBtn = document.querySelector('#resetBtn');
 var stopBtn = document.querySelector('#stopBtn');
-
-var trail=n=>n.toString().length<2?'0'+n:n;
-var secs = mins = 0;
-var running = false;
-var to;
-timerDiv.innerText=`${trail(mins)}:${trail(secs)}`;
-
-startBtn.addEventListener('click',function(e){
-	startBtn.disabled=true;
-	running = true; 
-	timer();
-});
-resetBtn.addEventListener('click',function(e){
-	mins = secs = 0;
-	timerDiv.innerText=`${trail(mins)}:${trail(secs)}`;
-});
-stopBtn.addEventListener('click',function(e){
-	clearTimeout(to);
-	startBtn.disabled=false;
-})
-
-function timer(){
-	secs++;
-	if(secs>59){
-		mins++;
-		secs=0;
-	}
-	timerDiv.innerText=`${trail(mins)}:${trail(secs)}`;
-	if(running)
-		to = setTimeout(timer,1000);
-	else
-		clearTimeout(to);
-}
-//setTimeout(timer, 1000);
