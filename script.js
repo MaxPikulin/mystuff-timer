d9787c2613ade58c89fa0588881c8008fb2f8500
MaxPikulin <trurl1000@gmail.com>
2017-08-12 19:48:21 +0100

    broke all

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 var timer = {
 	minutes: 0,
 	seconds: 0,
+	timestamp: 0,
 	show: function() {
-		var m=this.minutes+'',s=this.seconds+'';
-		if(m.length<2) m='0'+m;
-		if(s.length<2) s='0'+s;
-		return `${m}:${s}`;
+		return `${format(timer.minutes)}:${format(timer.seconds)}`;
+
+		function format(n) {
+			return (n+'').length<2?'0'+n:n;
+		}
+	},
+	calculate: function() {
+		if(timer.timestamp===0)
+			timer.timestamp=Date.now();
+		timer.seconds =  Math.round((Date.now()-timer.timestamp)/1000);
+		if(timer.seconds>59){
+		timer.minutes+=1;
+		timer.seconds=0;
+	}
+	console.log(timer.show());
 	}
 }
+
+setInterval(timer.calculate, 1000);
 
 var timerDiv = document.querySelector('#timerInner');
 var startBtn = document.querySelector('#startBtn');
