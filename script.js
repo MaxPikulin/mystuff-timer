ca31acc76d8a5f860a47f9cb2199b1f9036ed309
MaxPikulin <trurl1000@gmail.com>
2017-08-13 17:15:34 +0100

    New start n3 :smile:
    Make it simple.
    No objects, no fancy stuff. Just plain and boring funcs and global vars.

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-var timer = {
-	minutes: 0,
-	seconds: 0,
-	timestamp: 0,
-	show: function() {
-		return `${format(timer.minutes)}:${format(timer.seconds)}`;
 
-		function format(n) {
-			return (n+'').length<2?'0'+n:n;
-		}
-	},
-	calculate: function() {
-		if(timer.timestamp===0)
-			timer.timestamp=Date.now();
-		timer.seconds =  Math.round((Date.now()-timer.timestamp)/1000);
-		if(timer.seconds>59){
-		timer.minutes+=1;
-		timer.seconds=0;
-	}
-	console.log(timer.show());
-	}
-}
-
-setInterval(timer.calculate, 1000);
 
 var timerDiv = document.querySelector('#timerInner');
 var startBtn = document.querySelector('#startBtn');
