e2f06a9d49e6ed47fd3ea9c975d13b306f61772d
MaxPikulin <trurl1000@gmail.com>
2017-08-26 13:18:07 +0100

    No ms,  hours, some layout changes.

--- script.js
+++ script.js
@@ -1,65 +1,68 @@
-var minutes = 0,seconds = 0,fractionsOfSecond = 0, differenceInMs;
-var startTimestamp = 0,intervalID = null, paused = false;
+var hours,
+    minutes,
+    seconds,
+    differenceInMs;
+var startTimestamp = 0,
+    intervalID = null,
+    paused = false;
 
 function calculateTime() {
-	var currentTimestamp;
-	if(startTimestamp==0) {
-		startTimestamp=Date.now();
-	} else if(paused) {
-		paused=false;
-		startTimestamp=Date.now()-differenceInMs-100;
-	}
-	currentTimestamp=Date.now();
-	differenceInMs=currentTimestamp-startTimestamp;
-	minutes=Math.floor(differenceInMs/60000);
-	seconds=Math.floor((differenceInMs/1000)%60);
-	fractionsOfSecond=Math.floor((differenceInMs%1000)/100);
+    var currentTimestamp;
+    if (startTimestamp == 0) {
+        startTimestamp = Date.now();
+    } else if (paused) {
+        paused = false;
+        startTimestamp = Date.now() - differenceInMs - 100;
+    }
+    currentTimestamp = Date.now();
+    differenceInMs = currentTimestamp - startTimestamp;
+    hours = Math.floor(differenceInMs / 3600000);
+    minutes = Math.floor(differenceInMs / 60000) - hours * 60;
+    seconds = Math.floor((differenceInMs / 1000) % 60);
 }
 
 function formatZeros(n) {
-	var str = n.toString();
-	return str.length<2 ? '0'+str : str;
+    var str = n.toString();
+    return str.length < 2 ? '0' + str : str;
 }
 
 function displayTime(z) {
-	if(!z) {
-		calculateTime();
-		displayDiv.innerText=`${formatZeros(minutes)}:${formatZeros(seconds)}`;
-		displayFr.textContent=`.${fractionsOfSecond}`;
-	} else {
-		displayDiv.innerText=`00:00`;
-		displayFr.textContent=`.0`;
-	}
+    calculateTime();
+    //console.time('DOM manipulation'); 
+    //Looks like innerText works little bit faster than textContent.
+    hoursV.innerText = formatZeros(hours);
+    minutesV.innerText = formatZeros(minutes);
+    secondsV.innerText = formatZeros(seconds);
+    //console.timeEnd('DOM manipulation');
+
 }
 
 function startBtnHandler() {
-	if(intervalID===null) {
-		intervalID=setInterval(displayTime,100);
-	}
+    if (intervalID === null) {
+        intervalID = setInterval(displayTime, 1000);
+        displayTime();
+    }
 }
 
 function stopBtnHandler() {
-	clearInterval(intervalID);
-	intervalID=null;
-	paused=true;
+    clearInterval(intervalID);
+    intervalID = null;
+    paused = true;
 }
 
 function resetBtnHandler() {
-	startTimestamp=0;
-	differenceInMs=0;
-	displayTime(true);
+    startTimestamp = 0;
+    differenceInMs = 0;
+    displayTime();
 }
 
-//setInterval(displayTime,100);
-
-var displayDiv = document.querySelector('#timerInner');
+const hoursV = document.querySelector('#hours');
+const minutesV = document.querySelector('#minutes');
+const secondsV = document.querySelector('#seconds');
 var startBtn = document.querySelector('#startBtn');
 var resetBtn = document.querySelector('#resetBtn');
 var stopBtn = document.querySelector('#stopBtn');
-var displayFr = document.querySelector('#fr');
 
-startBtn.addEventListener('click',startBtnHandler);
-stopBtn.addEventListener('click',stopBtnHandler);
-resetBtn.addEventListener('click',resetBtnHandler);
-
-displayTime(true);
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+resetBtn.addEventListener('click', resetBtnHandler);
